2871dfba25d2217209eacb37a9a6c812318c01e2
Felipe <fdamasio17@gmail.com>
2024-02-18 17:05:52 -0300

    seção 4: 59. Usando Data Annotations

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -8,20 +8,22 @@ public class Produto
 {
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(80, ErrorMessage = "O nome de ter no máximo {1} caractéres",
+        MinimumLength = 5)]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, ErrorMessage = "A descriação deve ter no máximo {1} caractéres")]
     public string? Descricao { get; set; }
 
     [Required]
     [Column(TypeName = "decimal(10,2)")]
+    [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2} valores")]
     public decimal Preco { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, MinimumLength = 10)]
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
