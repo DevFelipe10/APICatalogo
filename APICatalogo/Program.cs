aebd9eaf192a0f2f1bbd7ad19ea56405e00962b1
Felipe <fdamasio17@gmail.com>
2024-03-03 14:59:26 -0300

    seção 4: 65. Middleware de tratamento global de exceções

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,5 @@
 using APICatalogo.Context;
+using APICatalogo.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -28,6 +29,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
