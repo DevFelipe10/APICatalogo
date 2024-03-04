0446bd3abfbecc92f06c0c2af966086de5b33c5a
Felipe <fdamasio17@gmail.com>
2024-03-04 19:25:09 -0300

    seção 4: 69. Criando um log customizado

---
+++ APICatalogo/Logging/CustomLoggerProviderConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Logging;
+
+//Classe de configuração do provedor de log customizado
+public class CustomLoggerProviderConfiguration
+{
+    public LogLevel logLevel { get; set; } = LogLevel.Warning;
+    public int EventId { get; set; } = 0;
+}
---
+++ APICatalogo/Logging/CustomerLogger.cs
@@ -0,0 +1,49 @@
+﻿namespace APICatalogo.Logging;
+
+public class CustomerLogger : ILogger
+{
+    readonly string loggerName;
+    readonly CustomLoggerProviderConfiguration loggerConfig;
+
+    public CustomerLogger(string name, CustomLoggerProviderConfiguration config)
+    {
+        loggerName = name;
+        loggerConfig = config;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == loggerConfig.logLevel;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
+                            Exception exception, Func<TState, Exception, string> formatter)
+    {
+        string message = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+
+        EscreverTextoNoArquivo(message);
+    }
+
+    private void EscreverTextoNoArquivo(string mensagem)
+    {
+        string caminhoArquivoLog = @"D:\log\apicatalogo\apicatalogo_log.txt";
+
+        using (StreamWriter streamWriter = new StreamWriter(caminhoArquivoLog, true))
+        {
+            try
+            {
+                streamWriter.WriteLine(mensagem);
+                streamWriter.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
