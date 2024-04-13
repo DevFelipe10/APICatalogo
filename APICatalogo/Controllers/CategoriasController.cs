245533eb915e5a72b5e7b41007d6a0e96b7dee14
Felipe <fdamasio17@gmail.com>
2024-04-13 15:30:19 -0300

    seção 6: 95. DTO - Mapeamento manual - CategoriaDTO

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,6 @@
-﻿using APICatalogo.Filters;
+﻿using APICatalogo.DTOs;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Filters;
 using APICatalogo.Models;
 using APICatalogo.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -32,18 +34,20 @@ public class CategoriasController : ControllerBase
 
     [HttpGet]
     [ServiceFilter(typeof(ApiLoggingFilter))]
-    public ActionResult<IEnumerable<Categoria>> Get()
+    public ActionResult<IEnumerable<CategoriaDTO>> Get()
     {
-        var categoria = _uof.CategoriaRepository.GetAll();
+        var categorias = _uof.CategoriaRepository.GetAll();
 
-        if (categoria is null)
-            throw new ArgumentNullException(nameof(categoria));
+        if (categorias is null)
+            return NotFound("Não existem categorias...");
 
-        return Ok(categoria);
+        var categoriasDto = categorias.ToCategoriaDTOList();
+
+        return Ok(categoriasDto);
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
-    public ActionResult<Categoria> Get(int id)
+    public ActionResult<CategoriaDTO> Get(int id)
     {
         var categoria = _uof.CategoriaRepository.Get(c => c.CategoriaId == id);
 
@@ -53,40 +57,50 @@ public class CategoriasController : ControllerBase
             return NotFound($"Produto com id = {id} não encontrado...");
         }
 
-        return Ok(categoria);
+        var categoriaDto = categoria.ToCategoriaDto();
+
+        return Ok(categoriaDto);
     }
 
     [HttpPost]
-    public ActionResult<Categoria> Post(Categoria categoria)
+    public ActionResult<CategoriaDTO> Post(CategoriaDTO categoriaDto)
     {
-        if (categoria is null)
+        if (categoriaDto is null)
         {
             return BadRequest("Objeto categoria é nulo");
         }
 
+        var categoria = categoriaDto.ToCategoria();
+
         var categoriaCriada = _uof.CategoriaRepository.Create(categoria);
         _uof.Commit();
 
+        var novaCategoriaDto = categoriaCriada.ToCategoriaDto();
+
         return new CreatedAtRouteResult("ObterCategoria",
-            new { id = categoriaCriada.CategoriaId }, categoriaCriada);
+            new { id = novaCategoriaDto.CategoriaId }, novaCategoriaDto);
     }
 
     [HttpPut("{id:int}")]
-    public ActionResult<Categoria> Put(int id, Categoria categoria)
+    public ActionResult<CategoriaDTO> Put(int id, CategoriaDTO categoriaDto)
     {
-        if (id != categoria.CategoriaId)
+        if (id != categoriaDto.CategoriaId)
         {
             return BadRequest($"O id = {id} informado não é igual ao objeto categoria");
         }
 
-        _uof.CategoriaRepository.Update(categoria);
+        var categoria = categoriaDto.ToCategoria();
+
+        var categoriaAtualizada = _uof.CategoriaRepository.Update(categoria);
         _uof.Commit();
 
-        return Ok(categoria);
+        var categoriaAtualizadaDto = categoriaAtualizada.ToCategoriaDto();
+
+        return Ok(categoriaAtualizadaDto);
     }
 
     [HttpDelete("{id:int}")]
-    public ActionResult<Produto> Delete(int id)
+    public ActionResult<CategoriaDTO> Delete(int id)
     {
         var categoria = _uof.CategoriaRepository.Get(c => c.CategoriaId == id);
 
@@ -98,6 +112,8 @@ public class CategoriasController : ControllerBase
         var categoriaExcluida = _uof.CategoriaRepository.Delete(categoria);
         _uof.Commit();
 
-        return Ok(categoriaExcluida);
+        var categoriaExcluidaDto = categoriaExcluida.ToCategoriaDto();
+
+        return Ok(categoriaExcluidaDto);
     }
 }
